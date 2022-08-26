f559a8eaa114dd4bb306f8af460a8eb705f811b3
Jayesh Poonia <103627572+Jayesh260@users.noreply.github.com>
2022-08-27 00:46:23 +0530

    Create 2.cpp

---
+++ OODP/week 2/2.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=1000;i<2000;i++){
+        if(i%8==0&&i%5==0){
+            cout<<i<<" ";
+        }
+    }
+    return 0;
+}
