078a6f284064f2aa85e2d925dbc6203c6f141e53
Jayesh Poonia <103627572+Jayesh260@users.noreply.github.com>
2022-08-27 00:49:28 +0530

    Create 3.cpp

---
+++ OODP/week 2/3.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num,power;
+    cout<<"enter the number"<<endl;
+    cin>>num;
+    cout<<"enter the power"<<endl;
+    cin>>power;
+    cout<<pow(num,power)<<endl;
+    return 0;
+}
