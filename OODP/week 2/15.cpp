9afdfbc2bf8dcdc639130a3d247b4cde8fc1529e
Jayesh Poonia <103627572+Jayesh260@users.noreply.github.com>
2022-08-27 14:21:04 +0530

    Create 15.cpp

---
+++ OODP/week 2/15.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    for(int i=9;i>0;i--){
+        for(int j=i;j>0;j--){
+            cout<<i;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
