6eb493f886049f55e27ff0f80220ff4aedd45f5d
Jayesh Poonia <103627572+Jayesh260@users.noreply.github.com>
2022-08-27 14:26:15 +0530

    Create 14.cpp

---
+++ OODP/week 2/14.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int first,second;
+    cout<<"enter the numbers"<<endl;
+    cin>>first>>second;
+    if(first+second>105&&first+second<200){
+        cout<<"200"<<endl;
+    }
+    else
+    cout<<first+second<<endl;
+    return 0;
+}
