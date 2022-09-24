92aff9fd2f6fb92b245727ea8e4f780021e58bcf
Jayesh Poonia <103627572+Jayesh260@users.noreply.github.com>
2022-09-24 16:53:39 +0530

    Create 7.cpp

---
+++ OODP/Week 5/7.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+class func{
+    public:
+    void pal(char c[]);
+};
+void func::pal(char c[]){
+        int l=strlen(c);
+        char b[10];
+        int i,j;
+        for(i=0,j=l-1;i<l;i++,j--){
+            b[i]=c[j];
+        }
+        b[i]=' ';
+        if(strcmp(c,b)==0){
+            cout<<"\nThe entered text is palindrome";
+        }
+        else{
+            cout<<"\n the entered text is not a palindrome";
+        }
+}
+int main()
+{
+        char c[10];
+        int k,r,v;
+        func p;
+        cout<<"\nEnter the string to be checked"<<endl;
+        cin>>c;
+        p.pal(c);
+        return 0;
+}
