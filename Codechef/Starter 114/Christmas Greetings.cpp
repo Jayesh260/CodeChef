7a41052fa93774ca05e6b036696cff333e13a173
Jayesh Poonia <103627572+Jayesh260@users.noreply.github.com>
2024-01-01 21:14:42 +0530

    Create Christmas Greetings.cpp

---
+++ Codechef/Starter 114/Christmas Greetings.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int lli;
+const int mod=1e9 +7;
+// #define rng_init mt19937 rng(chrono::steady_clock::now().time_since_epoch().count())
+// #define rng_seed(x) mt19937 rng(x)
+// #define sz(x) (int)(x).size()
+// const ing MXN =1e5+5, INF=1e9+5; 
+void solve(){
+	int n;
+	cin>>n;
+	if(n==25)
+	cout<<"CHRISTMAS"<<endl;
+	else
+	cout<<"ORDINARY"<<endl;
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.tie(NULL);
+	int t=1;
+	// cin>>t;
+	while(t--)
+	solve();
+	return 0;
+}
