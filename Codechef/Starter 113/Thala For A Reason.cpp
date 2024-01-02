f4026b90865adfc83914a6727585adaeecd398c3
Jayesh Poonia <103627572+Jayesh260@users.noreply.github.com>
2024-01-02 08:40:07 +0530

    Create Thala For A Reason.cpp

---
+++ Codechef/Starter 113/Thala For A Reason.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int lli;
+void solve(){
+	
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.tie(NULL);
+	int t;
+	cin>>t;
+	if(t==7)
+	cout<<"THALA";
+	else
+	cout<<"SADGE";
+	return 0;
+}
